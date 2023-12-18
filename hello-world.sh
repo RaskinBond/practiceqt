b70ad1ce6ad811f3c457082f814ab4e0e98bd33d
nykzaR <nykzar@outlook.com>
2023-12-18 07:19:57 +0530

    added 2files

---
+++ hello-world.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+###########S#c#r#i#p#t######H#e#a#d#e#r####################
+# Author: Raskin B.
+# Version: v1.0.0
+# Date: 11-Dec-2022
+# Description: This is my first shell script
+# Usage: ./helloworld.sh
+###########################################################
+
+
+# Print the text to the terminal
+echo "Hello World!"
+
+# Just experimenting
+echo "Hey $(whoami)!"
+
+# Using Interpolation
+my_name="Raskin"
+org_name="Swagg"
+
+echo "Hi, This is ${my_name}, working with ${org_name}."
+
+# Arithmetic operations
+number=3
+echo "Sum of 3 & 2 = $(($number + 2))"
---
+++ user-input.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+###########S#c#r#i#p#t######H#e#a#d#e#r####################
+# Author: Raskin B.
+# Version: v1.0.0
+# Date: 12-Dec-2022
+# Description: This is a shell script demonstrating user inputs
+# Usage: ./user-input.sh
+###########################################################
+
+name=${1}
+
+echo "Hello ${name}!"
+
+echo "0 = ${0}  1 = ${1}"
