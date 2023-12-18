5a4dd6590f0e7ae563676b6169c089d728d58fcd
RaskinBond <bond.raskin91@gmail.com>
2023-12-18 21:45:14 +0530

    Added scripts

---
+++ scripts/script1.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+###########S#c#r#i#p#t######H#e#a#d#e#r####################
+# Author: Raskin B.
+# Version: v1.0.0
+# Date: 15-Dec-2022
+# Description: This is an interactive shell script asking questions to the user
+# Usage: ./script1.sh
+###########################################################
+
+thanos="Avengers Infinity"
+echo "This is from script 1 thanos= ${thanos}"
+export thanos
+./script2.sh
---
+++ scripts/script2.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+###########S#c#r#i#p#t######H#e#a#d#e#r####################
+# Author: Raskin B.
+# Version: v1.0.0
+# Date: 15-Dec-2022
+# Description: This is an interactive shell script asking questions to the user
+# Usage: ./script2.sh
+###########################################################
+
+ant-man="Avengers End Game"
+echo "This is script2"
+echo "${ant-man} is the other half of ${thanos}"
